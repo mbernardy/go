5486fd8bfdbc0136aba5a95ef91eb1c30f282c17
Max Bernardy <max.bernardy@gmail.com>
2013-07-26 23:40:21 -0400

    Draw pieces, alternating colors

--- public/js/index/Board.js
+++ public/js/index/Board.js
@@ -1,51 +1,86 @@
 function Board(){
+	"use strict";
 
 	var _$root = $('<canvas id="board" width="500" height="500"></canvas>');
+	var _ctx;
+	var _xStep;
+	var _yStep;
 
-	var rows = cols = 9;
-	//pixels between edge of game board and edge of canvas
-	var padding = 20;
+	var rows = 9;
+	var cols = 9; 
+	var padding;
 
-	function _getColSpacing(){
-		var boardWidth = _$root.width() - 2 * padding;
-		return boardWidth / (cols - 1);
+
+	function _init(){
+		var elem = _$root.get(0);
+		_ctx = elem.getContext('2d');
+
+
+
+		_bindEvents();
 	}
 
-	function _getRowSpacing(){
-		var boardHeight = _$root.height() - 2 * padding;
-		return boardHeight / (rows - 1);
+	function _bindEvents(){
+
+		var plays = 0;
+		var orange = "#FFAE00";
+		var black  = "#000000"
+		_$root.on('click', function(e){
+			var x, y;
+			x = e.pageX - padding;
+			var col = Math.floor(x / _xStep);
+
+			y = e.pageY - padding;
+			var row = Math.floor(y / _yStep);
+
+			var color = plays++ % 2 == 0 ? black : orange;
+			_drawPiece(row, col, color);
+
+		});
+	}
+
+	function _drawPiece(row, col, color){
+		console.log("Trying to draw piece at ", row, col);
+
+		var x = padding + (col * _xStep);
+		var y = padding + (row * _yStep);
+		_ctx.beginPath();
+		_ctx.arc(x, y, _xStep/2, 0, 2 * Math.PI );
+		_ctx.fillStyle = color;
+		_ctx.fill();
+
 	}
 
 
 	this.draw = function(){
-		var elem = _$root.get(0);
-		var ctx = elem.getContext('2d');
+
+		_xStep = _$root.width() / cols;
+		_yStep = _$root.height() / rows;
+		padding = _xStep / 2;
+
 
 		var leftX = padding;
 		var rightX = _$root.width() - padding;
 		var topY = padding;
 		var bottomY = _$root.height() - padding;
 
-		var yStep = _getRowSpacing();
-		var xStep = _getColSpacing();
-
 		//draw the horizontal lines
 		var y = topY;
 		for(var i=0; i < rows; i++){
-			ctx.moveTo(leftX, y);
-			ctx.lineTo(rightX, y);
-			ctx.stroke();
+			_ctx.moveTo(leftX, y);
+			_ctx.lineTo(rightX, y);
+			_ctx.stroke();
 
-			y += yStep;
+			y += _yStep;
 		}
 		
 		var x = leftX;
 		for(var i=0; i < cols; i++){
-			ctx.moveTo(x, topY);
-			ctx.lineTo(x, bottomY);
-			ctx.stroke();
+			_ctx.moveTo(x, topY);
+			_ctx.lineTo(x, bottomY);
+			_ctx.stroke();
 
-			x += xStep;
+			x += _xStep;
 		}
 
 	}
@@ -53,5 +88,7 @@ function Board(){
 	this.get$Root = function(){
 		return _$root;
 	}
+
+	_init();
 
 }
