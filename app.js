c7a250121e45239eaae8ce6f96659e871061a512
Max Bernardy <max.bernardy@gmail.com>
2013-07-26 22:41:43 -0400

    Changed to jade for rendering

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
-
-
 var app = express();
+app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + "/public/js"));
+
+app.get('/', function(req, res){
+	res.render('index');
+})
 
 app.listen(3000);
